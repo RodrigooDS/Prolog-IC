420c9195ab8ccaadcff3bdf7da7de1109cdd0260
Gonzalo Espinoza <gonzalo.e.espinoza.o@gmail.com>
2020-11-05 22:31:40 -0300

    Creacion codigo Prolog

---
+++ prologcomplicado.pl
@@ -0,0 +1,47 @@
+miente(alberto,lunes).
+miente(alberto,martes).
+miente(alberto,miercoles).
+
+miente(luis,viernes).
+miente(luis,sabado).
+miente(luis,domingo).
+
+miente(carlos,miercoles).
+miente(carlos,jueves).
+miente(carlos,viernes).
+
+verdad(alberto,jueves).
+verdad(alberto,viernes).
+verdad(alberto,sabado).
+verdad(alberto,domingo).
+
+verdad(luis,lunes).
+verdad(luis,martes).
+verdad(luis,miercoles).
+verdad(luis,jueves).
+
+verdad(carlos,sabado).
+verdad(carlos,domingo).
+verdad(carlos,lunes).
+verdad(carlos,martes).
+
+nombre(alberto, alberto).
+nombre(carlos, carlos).
+nombre(luis, luis).
+
+nonombre(alberto,luis).
+nonombre(alberto,carlos).
+
+nonombre(luis,alberto).
+nonombre(luis,carlos).
+
+nonombre(carlos,alberto).
+nonombre(carlos,luis).
+
+mañana(lunes,martes).
+mañana(martes,miercoles).
+mañana(miercoles,jueves).
+mañana(jueves,viernes).
+mañana(viernes,sabado).
+mañana(sabado,domingo).
+mañana(domingo,lunes).
